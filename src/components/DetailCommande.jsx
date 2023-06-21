188e59cfc4e80b7f15f3bc551410dde15b1ab424
Bibibush <rlawlsgus125@gmail.com>
2023-06-21 12:06:00 +0200

    detail commande 디자인 중

--- src/components/DetailCommande.jsx
+++ src/components/DetailCommande.jsx
@@ -3,12 +3,14 @@ import { useState, useCallback, useEffect } from "react";
 import axios from "axios";
 import ClientDownload from "./ClientDownload";
 import { Button, Form } from "react-bootstrap";
+import ReactDatePicker from "react-datepicker";
 
 export default function DetailCommande() {
   const [searchParams] = useSearchParams();
   const id = searchParams.get("id");
   const [successInfo, setSuccessInfo] = useState({});
   const [excelUpdate, setExcelUpdate] = useState(false);
+  const [dateUpdate, setDateUpdate] = useState(false);
   const [files, setFiles] = useState([]);
   const onChangeupload = useCallback((event) => {
     setFiles(event.target.files);
@@ -38,9 +40,9 @@ export default function DetailCommande() {
           {successInfo.order_number}
         </div>
         <div className="excel">
-          <p>Fiche de votre commande</p>
+          <p>Fiche de votre commande: </p>
           {excelUpdate ? (
-            <>
+            <div className="client_upload">
               <Form.Group controlId="formFileLg" className="mb-3">
                 <Form.Label>Enregistrez votre fiche en bas</Form.Label>
                 <Form.Control
@@ -51,17 +53,19 @@ export default function DetailCommande() {
                 />
               </Form.Group>
               <Button
+                variant="danger"
                 onClick={() => {
                   setExcelUpdate(false);
                 }}
               >
-                Enregistrez
+                Annuler
               </Button>
-            </>
+            </div>
           ) : (
             <div className="client_download">
               <ClientDownload />
               <Button
+                variant="secondary"
                 onClick={() => {
                   setExcelUpdate(true);
                 }}
@@ -74,7 +78,31 @@ export default function DetailCommande() {
         </div>
         <div className="date">
           <p>La date que vous recevoir la commande</p>
-          <div className="date_commande">{successInfo.date}</div>
+          {dateUpdate ? (
+            <div className="date_change">
+              <ReactDatePicker />
+              <Button
+                variant="danger"
+                onClick={() => {
+                  setDateUpdate(false);
+                }}
+              >
+                Annuler
+              </Button>
+            </div>
+          ) : (
+            <div className="date_commande">
+              <h3>{successInfo.date}</h3>
+              <Button
+                variant="secondary"
+                onClick={() => {
+                  setDateUpdate(true);
+                }}
+              >
+                Changez la date
+              </Button>
+            </div>
+          )}
         </div>
       </div>
       <div className="commande_adresse"></div>
