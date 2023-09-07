80044e6b16bfe58e13682e5a8a0096358bd308b7
Bibibush <rlawlsgus125@gmail.com>
2023-09-07 16:48:00 +0200

    Logos

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -2,18 +2,24 @@ export default function Logos() {
   return (
     <section className="logos">
       <div className="logos_flex">
-        <img src="images/logo artisan.png" alt="" />
-        <img src="images/나뭇잎.png" alt="" />
-        <img src="images/모래시계.png" alt="" />
-        <img src="images/돋보기.png" alt="" />
-        <img src="images/France viande.png" alt="" />
+        <div className="logos_flex_rond">
+          <img src="images/logo artisan.png" alt="" />
+        </div>
+        <div className="logos_flex_rond">
+          <img src="images/나뭇잎.png" alt="" />
+        </div>
+        <div className="logos_flex_rond">
+          <img src="images/모래시계.png" alt="" />
+        </div>
+        <div className="logos_flex_rond">
+          <img src="images/돋보기.png" alt="" />
+        </div>
       </div>
       <div className="logos_desc">
         <h2>Artisan Français</h2>
         <h2>Fleur Naturelle</h2>
         <h2>Affinage Lent</h2>
         <h2>Viandes Rigoureusement Sélectionnées</h2>
-        <h2>Viandes Françaises Sélectionnées</h2>
       </div>
     </section>
   );
