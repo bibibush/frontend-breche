58239b0bda8b1a65c7efcd249446f454a8a71708
Bibibush <rlawlsgus125@gmail.com>
2023-09-25 17:20:06 +0200

    comment CSE

--- src/components/HowToOrder.jsx
+++ src/components/HowToOrder.jsx
@@ -1,7 +1,7 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Download from "../components/Download";
 import "../styles/Download.css";
-import { Button } from "react-bootstrap";
+import { Button, ButtonGroup, Modal } from "react-bootstrap";
 import axios from "axios";
 
 export default function HowToOrder() {
@@ -21,18 +21,58 @@ export default function HowToOrder() {
     getme();
   }, [getme]);
 
+  const [show, setShow] = useState(false);
+
   return (
     <section className="order_how">
+      <div className="steps">
+        <div className="step">
+          <div className="step_num">Etape 1</div>
+          <div className="desc">
+            Téléchargez 'MODELE Tableau CSE 2022.xlsx' fiche
+          </div>
+        </div>
+        <div className="step">
+          <div className="step_num">Etape 2</div>
+          <div className="desc">Rempliez votre commande dans le fiche</div>
+        </div>
+        <div className="step">
+          <div className="step_num">Etape 3</div>
+          <div className="desc">
+            Continuez finir votre commande
+            <br /> comme vous appuyez le bouton 'Aller commander'
+          </div>
+        </div>
+      </div>
       <Download />
-      <Button
-        onClick={() => {
-          window.location.href = "/commande";
-        }}
-        variant="warning"
+      <ButtonGroup size="lg">
+        <Button
+          onClick={() => {
+            window.location.href = "/commande";
+          }}
+        >
+          Aller commander
+        </Button>
+        <Button
+          onClick={() => {
+            setShow(true);
+          }}
+        >
+          Guide pour remplir le fiche
+        </Button>
+      </ButtonGroup>
+      <Modal
+        show={show}
         size="lg"
+        onHide={() => {
+          setShow(false);
+        }}
       >
-        Aller commander
-      </Button>
+        <Modal.Header closeButton>
+          <Modal.Title>Guide pour remplir le fiche</Modal.Title>
+          <Modal.Body></Modal.Body>
+        </Modal.Header>
+      </Modal>
     </section>
   );
 }
